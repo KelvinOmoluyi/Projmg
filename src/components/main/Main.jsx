c252a13ab1953d1612116e8df5d9871410bc0aae
KelvinOmoluyi <omoluyikelvin14@gmail.com>
2025-06-18 23:15:59 +0100

    Main animation complete

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,43 +1,59 @@
-import { big } from 'framer-motion/client';
+import { filter } from 'framer-motion/client';
 import ArrowBtn from '../../assets/img/btn-arrow.png';
 import { motion } from "framer-motion";
 
 
 const Main = () => {
+    const pagesLinks = ["Features", "Integrations", "Reviews", "Contact"];
+
     const sentence1 = "Project Management has never been easier and compact.";
     const bigWords = sentence1.split("");
 
-    const smallWords = "";
+    const smallWords = "Organize your projects, chat with your team, and get things done — all from one simple workspace.";
 
 
     return (
-        <main className="main-section wrapper">
+        <main className="main-section wrapper" id="home">
         <header>
             <nav>
                 <div className="nav-logo">
-                    <h3>PROJ.MG</h3>
+                    <motion.div 
+                    initial={{ filter: "blur(10px)", opacity: 0, y: 20 }}
+                    whileInView={{ filter: "blur(0px)", opacity: 1, y: 0 }}
+                    transition={{ duration: 0.7 }}
+                    viewport={{ once: true, amount: 1 }}
+                    className="overflow-hidden-container-inner">       
+                        <a href="#home"><h3>PROJ.MG</h3></a>
+                    </motion.div>
                 </div>
                 <div className="nav-sections-link">
                     <ul>
-                        <li>
-                            <a href="#features"><p>Features</p></a>
-                        </li>
-                        <li>
-                            <a href="#integrations"><p>Integrations</p></a>
-                        </li>
-                        <li>
-                            <a href="#reviews"><p>Reviews</p></a>
-                        </li>
-                        <li>
-                            <a href="#footer"><p>Contact</p></a>
-                        </li>
+                        {pagesLinks.map((pagelink, index) => (
+                            <li key={index}>
+                                <a href={"#"+pagelink}>
+                                    <motion.p
+                                    className="upRevealText-inline"
+                                    initial={{ filter: "blur(10px)", opacity: 0, y: 50 }}
+                                    whileInView={{ filter: "blur(0px)", opacity: 1, y: 0 }}
+                                    transition={{ duration: 1, delay: 0.05*index }}
+                                    viewport={{ once: true, amount: 1 }}
+                                    >{pagelink}</motion.p>
+                                </a>
+                            </li>
+                        ))}
                     </ul>
                 </div>
                 <div className="cta-btn-div">
-                    <div className="cta-btn-01">
-                        <p>Start Today!</p>
-                        <img src={ArrowBtn} alt="button arrow" />
-                    </div>
+
+                    <motion.div
+                    initial={{ filter: "blur(10px)", opacity: 0, y: 20 }}
+                    whileInView={{ filter: "blur(0px)", opacity: 1, y: 0 }}
+                    transition={{ duration: 0.7 }}
+                    viewport={{ once: true, amount: 1 }}
+                    className="cta-btn-01">
+                            <p>Start Today!</p>
+                            <img src={ArrowBtn} alt="button arrow" />
+                    </motion.div>
                 </div>
             </nav>
         </header>
@@ -58,7 +74,7 @@ const Main = () => {
                 <div className="main-section-content-words-big-words h-text-gradient">
                         {bigWords.map((bigWord, index) => (
                             <motion.h1
-                            className="upRevealText"
+                            className="upRevealText-inline"
                             initial={{ filter: "blur(10px)", opacity: 0, y: 50 }}
                             whileInView={{ filter: "blur(0px)", opacity: 1, y: 0 }}
                             transition={{ duration: 0.7, delay: 0.02*index }}
@@ -69,33 +85,57 @@ const Main = () => {
                             </motion.h1>
                         ))}
                 </div>
+
                 <div className="main-section-content-words-small-words">
-                    <div className="overflow-hidden-container-inner">
-                        <motion.p
-                        initial={{ y:60 }}
-                        animate={{ y:0 }}
-                        transition={{ duration: 0.5, delay: 1.4 }}
-                        viewport={{ once: true }}
-                        >
-                            Organize your projects, chat with your team, and get things done — all from one simple workspace.
-                        </motion.p>
-                    </div>
+                    <motion.p
+                    className="upRevealText-block"
+                    initial={{ filter: "blur(10px)", opacity: 0, y: 50 }}
+                    whileInView={{ filter: "blur(0px)", opacity: 1, y: 0 }}
+                    transition={{ duration: 0.5, delay: 1.4 }}
+                    viewport={{ once: true, amount: 0.3 }}
+                    onViewportEnter={() => console.log("In view")}
+                    >
+                        {smallWords}
+                    </motion.p>
                 </div>
-                <motion.div className="cta-btn-div">
-                    <a className="cta-btn-02">
-                        <p>Start Free Trail</p>
-                        <img src={ArrowBtn} alt="button arrow" />
-                    </a>
-                    <a className="cta-btn-01">
-                        <p>Request Free Demo</p>
-                    </a>
-                </motion.div>
+
+                <div className="overflow-hidden-container-outer">                      
+                    <motion.div 
+                    initial={{ filter: "blur(10px)", opacity: 0, y: 50 }}
+                    whileInView={{ filter: "blur(0px)", opacity: 1, y: 0 }}
+                    transition={{ duration: 0.7, delay: 1.7 }}
+                    viewport={{ once: true, amount: 1 }}
+                    className="cta-btn-div"
+                    >             
+                        <a className="cta-btn-02">
+                            <p>Start Free Trail</p>
+                            <img src={ArrowBtn} alt="button arrow" />
+                        </a>
+                        <a className="cta-btn-01">
+                            <p>Request Free Demo</p>
+                        </a>
+                    </motion.div>
+                </div>
                 <div className="main-section-content-words-card-note">
-                    <p><span>No Credit Card Required</span> <span>•</span> <span>Cancel Everytime</span></p>
+                    <motion.div 
+                    initial={{ filter: "blur(10px)", opacity: 0, y: 50 }}
+                    whileInView={{ filter: "blur(0px)", opacity: 1, y: 0 }}
+                    transition={{ duration: 0.7, delay: 2 }}
+                    viewport={{ once: true, amount: 1 }}
+                    className="overflow-hidden-container-inner">     
+                        <p><span>No Credit Card Required</span> <span>•</span> <span>Cancel Everytime</span></p>
+                    </motion.div>
                 </div>
             </div>
-            <div className="main-section-content-dashboard">
-            </div>
+
+            <motion.div   
+            initial={{ filter: "blur(10px)", opacity: 0, y: 50 }}
+            whileInView={{ filter: "blur(0px)", opacity: 1, y: 0 }}
+            transition={{ duration: 0.7, delay: 1 }}
+            viewport={{ once: true, amount: 0 }}
+            className="main-section-content-dashboard">
+
+            </motion.div>
         </div>
     </main>
     );
