0dfe5118757391c8c2e1c7dc417f33512a037493
KelvinOmoluyi <omoluyikelvin14@gmail.com>
2025-06-19 00:11:27 +0100

    Day 2 code

--- src/components/integrations/Integrations.jsx
+++ src/components/integrations/Integrations.jsx
@@ -1,7 +1,13 @@
+import { motion } from 'framer-motion';
 import ArrowBtn from '../../assets/img/btn-arrow.png';
 import IntegrationsImg from '../../assets/img/integrations-img.png';
 
 const Integrations = () => {
+
+    const title = "Seamless integration with your favourite tools".split("");
+
+    const smallWords = "Seamlessly integrate with platforms like Zapier, WordPress, Shopify, Slack, Messenger, WhatsApp, Crisp, Intercom, and Zendesk."
+
     return (
             <section className="seamless-integration wrapper" id="integrations">
                 <div className="background-gradient"></div>
@@ -9,14 +15,39 @@ const Integrations = () => {
                 <div className="seamless-integration-content">
                     <div className="seamless-integration-content-heading">
                         <div className="seamless-integration-content-heading-big-words h-text-gradient">
-                            <h2>Seamless integration with your favourite tools</h2>
+                            {title.map((letter, index) => (
+                                <motion.h2
+                                className="upRevealElement-inline"
+                                initial={{ filter: "blur(10px)", opacity: 0, y: 40 }}
+                                whileInView={{ filter: "blur(0px)", opacity: 1, y: 0 }}
+                                transition={{ duration: 0.9, delay: 0.03*index }}
+                                viewport={{ once: true, amount: 1 }}
+                                key={index}
+                                >
+                                    {letter === " " ? "\u00A0" : letter}
+                                </motion.h2>
+                            ))}
+                                
                         </div>
+
                         <div className="seamless-integration-content-heading-small-words">
-                            <p>Seamlessly integrate with platforms like Zapier, WordPress, Shopify, 
-                                Slack, Messenger, WhatsApp, Crisp, Intercom, and Zendesk. 
-                            </p>
+                            <motion.p
+                            initial={{ filter: "blur(10px)", opacity: 0, y: 40 }}
+                            whileInView={{ filter: "blur(0px)", opacity: 1, y: 0 }}
+                            transition={{ duration: 0.9, delay: 0.9 }}
+                            viewport={{ once: true, amount: 1 }}
+                            className="upRevealElement-block"
+                            >
+                                {smallWords}
+                            </motion.p>
                         </div>
-                        <div className="cta-btn-div">
+
+                        <motion.div 
+                        initial={{ filter: "blur(10px)", opacity: 0, y: 50 }}
+                        whileInView={{ filter: "blur(0px)", opacity: 1, y: 0 }}
+                        transition={{ duration: 0.7, delay: 1.7 }}
+                        viewport={{ once: true, amount: 1 }}
+                        className="cta-btn-div">
                             <a className="cta-btn-02">
                                 <p>Start Free Trail</p>
                                 <img src={ArrowBtn} alt="button arrow" />
@@ -24,7 +55,7 @@ const Integrations = () => {
                             <a className="cta-btn-01">
                                 <p>Request Free Demo</p>
                             </a>
-                        </div>
+                        </motion.div>
                     </div>
 
                     <div className="seamless-integration-content-img">
