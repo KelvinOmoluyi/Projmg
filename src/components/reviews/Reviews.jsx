544ab17c4a2eb4191f4382e2cc49d1e440e91315
KelvinOmoluyi <omoluyikelvin14@gmail.com>
2025-06-19 14:19:18 +0100

    Finshed Version

--- src/components/reviews/Reviews.jsx
+++ src/components/reviews/Reviews.jsx
@@ -1,40 +1,114 @@
+import { motion } from 'framer-motion';
 import ClientImg from '../../assets/img/client1.png'
 
 const Reviews = () => {
+
+    const title = "3700+ Happy Customers".split("");
+
     return (    
         <section className="reviews wrapper h-text-gradient" id="reviews">
             <div className="reviews-content">
                 <div className="reviews-content-heading">
                     <div className="reviews-content-heading-tip">
-                        <h5>Read Customer’s Review</h5>
+                        <motion.h5
+                        initial={{ filter: "blur(10px)", opacity: 0, y: 40 }}
+                        whileInView={{ filter: "blur(0px)", opacity: 1, y: 0 }}
+                        transition={{ duration: 0.9, delay: 0 }}
+                        viewport={{ once: true, amount: 0 }}
+                        className="upRevealElement-block"
+                        >
+                            Read Customer’s Review
+                        </motion.h5>
                     </div>
                     <div className="reviews-content-heading-big-words">
-                        <h2>3700+ Happy Customers</h2>
+                        {title.map((letter, index) => (  
+                            <motion.h2
+                            initial={{ filter: "blur(10px)", opacity: 0, y: 40 }}
+                            whileInView={{ filter: "blur(0px)", opacity: 1, y: 0 }}
+                            transition={{ duration: 0.9, delay: 0.03*index }}
+                            viewport={{ once: true, amount: 1 }}
+                            key={index}
+                            className="upRevealElement-inline"
+                            >
+                                {letter === " " ? "\u00A0" : letter}
+                            </motion.h2>
+                        ))}
                     </div>
                 </div>
             </div>
-            <div className="reviews-content-reviews-container">
+            <motion.div 
+            initial={{ filter: "blur(10px)", opacity: 0 }}
+            whileInView={{ filter: "blur(0px)", opacity: 1 }}
+            transition={{ duration: 0.4, delay: 1 }}
+            viewport={{ once: true, amount: 0 }}
+            className="reviews-content-reviews-container"
+            >
                 <div className="review">
-                    <div className="review-img">
+                    <motion.div 
+                    initial={{ filter: "blur(10px)", opacity: 0, scale: 0.7 }}
+                    whileInView={{ filter: "blur(0px)", opacity: 1, scale: 1 }}
+                    transition={{ duration: 0.4, delay: 1.4 }}
+                    viewport={{ once: true, amount: 0 }}
+                    className="review-img"
+                    >
                         <img src={ClientImg} alt="client picture" />
-                    </div>
+                    </motion.div>
+
                     <div className="review-words">
                         <div className="background-gradient"></div>
 
-                        <h4>100% Best Software</h4>
-                        <div className="breakpoint1"></div>
-                        <p>Organize your projects, chat with your team, and get things done — all from one simple 
+                        <motion.h4
+                        initial={{ filter: "blur(10px)", opacity: 0, y: 30 }}
+                        whileInView={{ filter: "blur(0px)", opacity: 1, y: 0 }}
+                        transition={{ duration: 0.4, delay: 1.6 }}
+                        viewport={{ once: true, amount: 0 }}
+                        className="upRevealElement-block"
+                        >
+                            100% Best Software
+                        </motion.h4>
+
+                        <div 
+                        className="breakpoint1"></div>
+
+                        <motion.p
+                        initial={{ filter: "blur(10px)", opacity: 0, y: 50 }}
+                        whileInView={{ filter: "blur(0px)", opacity: 1, y: 0 }}
+                        transition={{ duration: 0.6, delay: 1.7 }}
+                        viewport={{ once: true, amount: 0 }}
+                        className="upRevealElement-block"
+                        >
+                            Organize your projects, chat with your team, and get things done — all from one simple 
                             workspace.Organize your projects, chat with your team, and get things done — all from 
                             one simple workspace.Organize your projects, chat with your team, and get things done — 
                             all from one simple workspace.
-                        </p>
+                        </motion.p>
+
                         <div className="breakpoint2"></div>
-                        <h5>Michael Johnas</h5>
-                        <p>Sales & Marketing, LLC</p>
+
+                        <motion.h5
+                        initial={{ filter: "blur(10px)", opacity: 0, y: 30 }}
+                        whileInView={{ filter: "blur(0px)", opacity: 1, y: 0 }}
+                        transition={{ duration: 0.6, delay: 1.7 }}
+                        viewport={{ once: true, amount: 0 }}
+                        className="upRevealElement-block"
+                        >
+                            Michael Johnas
+                        </motion.h5>
+
+                        <motion.p
+                        initial={{ filter: "blur(10px)", opacity: 0, y: 30 }}
+                        whileInView={{ filter: "blur(0px)", opacity: 1, y: 0 }}
+                        transition={{ duration: 0.6, delay: 1.8 }}
+                        viewport={{ once: true, amount: 0 }}
+                        className="upRevealElement-block"
+                        >
+                            Sales & Marketing, LLC
+                        </motion.p>
+
                         <div className='line'></div>
                     </div>
                 </div>
-            </div>
+            </motion.div>
         </section>
     );
 }
