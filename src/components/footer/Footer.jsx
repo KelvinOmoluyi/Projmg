39665d2b322a97307fed51fa6f58d0346b02c5b3
KelvinOmoluyi <omoluyikelvin14@gmail.com>
2025-06-19 14:18:31 +0100

    Finshed Version

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,36 +1,78 @@
+import { motion } from "framer-motion";
+
 const Footer = () => {
     return (
         <footer className="wrapper" id="footer">
             <div className="footer-contents">
                 <div className="footer-contents-company-logo">
-                    <h3>PROJ.MG</h3>
+                    <motion.h3
+                    initial={{ filter: "blur(10px)", opacity: 0, y: 40 }}
+                    whileInView={{ filter: "blur(0px)", opacity: 1, y: 0 }}
+                    transition={{ duration: 0.9, delay: 0 }}
+                    viewport={{ once: true, amount: 0 }}
+                    className="upRevealElement-block"
+                    >
+                        PROJ.MG
+                    </motion.h3>
                 </div>
                 <div className="footer-contents-links-and-summary">
                     <div className="column">
-                        <div className="data heading"><p>Product</p></div>
-                        <div className="data"><p>Pricing & Plans</p></div>
-                        <div className="data"><p>Product Features</p></div>
-                        <div className="data"><p>Integration</p></div>
-                        <div className="data"><p>Integration Details</p></div>
+                        <motion.div 
+                        initial={{ opacity: 0, x: -100 }}
+                        whileInView={{ opacity: 1, x: 0 }}
+                        transition={{ duration: 0.4, delay: 0.8 }}
+                        viewport={{ once: true, amount: 0 }}
+                        className="inner-column"
+                        >
+                            <div className="data heading"><p>Product</p></div>
+                            <div className="data"><p>Pricing & Plans</p></div>
+                            <div className="data"><p>Product Features</p></div>
+                            <div className="data"><p>Integration</p></div>
+                            <div className="data"><p>Integration Details</p></div>     
+                        </motion.div>
+                    </div> 
+                    <div className="column">
+                        <motion.div 
+                        initial={{ opacity: 0, x: -100 }}
+                        whileInView={{ opacity: 1, x: 0 }}
+                        transition={{ duration: 0.4, delay: 1.1 }}
+                        viewport={{ once: true, amount: 0 }}
+                        className="inner-column"
+                        >
+                            <div className="data heading"><p>Company</p></div>
+                            <div className="data"><p>About</p></div>
+                            <div className="data"><p>Product Features</p></div>
+                            <div className="data"><p>Changelog</p></div>
+                            <div className="data"><p>Contact</p></div>   
+                        </motion.div>
                     </div>
                     <div className="column">
-                        <div className="data heading"><p>Company</p></div>
-                        <div className="data"><p>About</p></div>
-                        <div className="data"><p>Product Features</p></div>
-                        <div className="data"><p>Changelog</p></div>
-                        <div className="data"><p>Contact</p></div>
+                        <motion.div 
+                        initial={{ opacity: 0, x: -100 }}
+                        whileInView={{ opacity: 1, x: 0 }}
+                        transition={{ duration: 0.4, delay: 1.25 }}
+                        viewport={{ once: true, amount: 0 }}
+                        className="inner-column"
+                        >
+                            <div className="data heading"><p>Resources</p></div>
+                            <div className="data"><p>Blog</p></div>
+                            <div className="data"><p>Blog Details</p></div>
+                            <div className="data"><p>Changelog</p></div> 
+                        </motion.div>
                     </div>
                     <div className="column">
-                        <div className="data heading"><p>Resources</p></div>
-                        <div className="data"><p>Blog</p></div>
-                        <div className="data"><p>Blog Details</p></div>
-                        <div className="data"><p>Changelog</p></div>
-                    </div>
-                    <div className="column">
-                        <div className="data heading"><p>Services</p></div>
-                        <div className="data"><p>Book A Demo</p></div>
-                        <div className="data"><p>Contact</p></div>
-                        <div className="data"><p>404</p></div>
+                        <motion.div 
+                        initial={{ opacity: 0, x: -100 }}
+                        whileInView={{ opacity: 1, x: 0 }}
+                        transition={{ duration: 0.4, delay: 1.35 }}
+                        viewport={{ once: true, amount: 0 }}
+                        className="inner-column"
+                        >
+                            <div className="data heading"><p>Services</p></div>
+                            <div className="data"><p>Book A Demo</p></div>
+                            <div className="data"><p>Contact</p></div>
+                            <div className="data"><p>404</p></div>
+                        </motion.div>
                     </div>
                 </div>
 
