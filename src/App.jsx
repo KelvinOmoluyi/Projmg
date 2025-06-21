69806b91bb193f85300b0b0fe8c4453c0cb78857
KelvinOmoluyi <omoluyikelvin14@gmail.com>
2025-06-21 21:57:58 +0100

    Implemented Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './css/reset.css'
-import './css/main.css'
+import './css/Main.css'
 import './css/App.css'
 import Main from './components/main/Main'
 import Clients from './components/clients/Clients'
@@ -8,18 +8,36 @@ import Integrations from './components/integrations/Integrations'
 import Reviews from './components/reviews/Reviews'
 import CallToAction from './components/call-to-action/CallToAction'
 import Footer from './components/footer/Footer'
+import Pricing from './components/pricing/Pricing'
+
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import NotFound from './components/notfound/NotFound'
 
 function App() {
 
   return (
     <>
-      <Main />
-      <Clients />
-      <Features />
-      <Integrations />
-      <Reviews />
-      <CallToAction />
-      <Footer />
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Main />
+            <Clients />
+            <Features />
+            <Integrations />
+            <Reviews />
+            <CallToAction />
+            <Footer />
+          </Route>
+          
+          <Route exact path="/pricing">
+            <Pricing  />
+          </Route>
+          
+          <Route path="*">
+            <NotFound />
+          </Route>
+        </Switch>
+      </Router>
     </>
   )
 }
