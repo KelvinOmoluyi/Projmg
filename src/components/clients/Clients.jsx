a29bee99c2c315a73afe3432da5202e2e6dd7df7
KelvinOmoluyi <omoluyikelvin14@gmail.com>
2025-06-21 21:57:32 +0100

    Perfecting Animations

--- src/components/clients/Clients.jsx
+++ src/components/clients/Clients.jsx
@@ -7,7 +7,7 @@ import LogoImg5 from '../../assets/img/business-logo5.png'
 import { motion } from 'framer-motion';
 
 const Client = () => {
-    const title = "Trusted by 45,000+ founders & business owners".split("");
+    const title = "Trusted by 45,000+ founders & business owners".split(" ").map(word => word.split(""));
 
     const Logos = [LogoImg1, LogoImg2, LogoImg3, LogoImg4, LogoImg5];
     
@@ -16,20 +16,24 @@ const Client = () => {
                 <div className="business-owners-content">
                     <div className="business-owners-content-scroll-container">
                         <div className="business-owners-content-heading-words">
-                            <h3>
-                                {title.map((word, index) => (
-                                    <motion.span
-                                    className="upRevealElement-inline"
-                                    initial={{ filter: "blur(10px)", opacity: 0, y: 40 }}
-                                    whileInView={{ filter: "blur(0px)", opacity: 1, y: 0 }}
-                                    transition={{ duration: 0.7, delay: 0.02*index }}
-                                    viewport={{ once: true, amount: 1 }}
-                                    key={index}
-                                    >
-                                        {word === " " ? "\u00a0" : word}
-                                    </motion.span>
-                                ))}
-                            </h3>
+                            {title.map((word, wordIndex) => (
+                                <h3
+                                className="word-wrapper" key={wordIndex}
+                                >
+                                    {word.map((letter, letterIndex) => (
+                                        <motion.span
+                                        key={letterIndex}
+                                        initial={{ filter: "blur(10px)", opacity: 0, y: 40 }}
+                                        whileInView={{ filter: "blur(0px)", opacity: 1, y: 0 }}
+                                        transition={{ duration: 0.7, delay: 0.015 * (wordIndex * 13 + letterIndex) }}
+                                        viewport={{ once: true, amount: 1 }}
+                                        className="upRevealElement-inline"
+                                        >
+                                            {letter}
+                                        </motion.span>
+                                    ))}
+                                </h3>
+                            ))}
                         </div>
                         <div className="business-owners-content-businesses-list">
                             {Logos.map((logo) => (
