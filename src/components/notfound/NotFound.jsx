64447d3d1a708fc91ca55edf44fd9a2700214e16
KelvinOmoluyi <omoluyikelvin14@gmail.com>
2025-06-21 21:55:36 +0100

    Created

--- src/components/notfound/NotFound.jsx
+++ src/components/notfound/NotFound.jsx
@@ -1,8 +1,24 @@
+import '../../css/Main.css'
+import '../../css/NotFound.css';
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
+import ArrowBtn from '../../assets/img/btn-arrow.png';
+
 const NotFound = () => {
     return (
-        <div>
-            <h1>Page Not Found</h1>
-        </div>
+        <section className="notfound-section">
+            <div className="words">
+                <h1>OOPS, Page Not Found :( </h1>
+            </div>
+            
+            <div className="cta-btn-div">
+                <Link to="/">
+                    <div className="cta-btn-02">
+                        <p>Go Back Home!</p>
+                        <img src={ArrowBtn} alt="button arrow" />
+                    </div>
+                </Link>
+            </div>
+        </section>
     );
 }
  
