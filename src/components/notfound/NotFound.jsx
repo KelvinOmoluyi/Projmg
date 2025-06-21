01e710a2f929f67f0835f15d8d5d611de3e6c809
KelvinOmoluyi <omoluyikelvin14@gmail.com>
2025-06-21 23:18:26 +0100

    Commit before deployment

--- src/components/notfound/NotFound.jsx
+++ src/components/notfound/NotFound.jsx
@@ -1,4 +1,4 @@
-import '../../css/Main.css'
+import '../../css/main.css'
 import '../../css/NotFound.css';
 import { Link } from "react-router-dom/cjs/react-router-dom.min";
 import ArrowBtn from '../../assets/img/btn-arrow.png';
